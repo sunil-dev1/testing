e7dd095061f1700a73c6c05eb7eb2cec4a87d16a
Eleve -  Sunil K <ss_apple@ip-192-168-1-206.ap-south-1.compute.internal>
2023-01-19 16:27:40 +0530

    dev branch created

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-console.log('hello worlting');
-console.log('hello world Greeting');
-console.log('hello world Greeting');
-console.log('hello world Greeting Day');
-console.log('hello world Greeting Day');
-console.log('hello world Greeting Day');
-console.log('hello world 2');
-console.log('hello world 2');
-console.log('hey I am Testing');
+console.log("I am dev branch");
+console.log("changing in dev branch");
