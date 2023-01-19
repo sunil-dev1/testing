7e9ce75d90055334d1759ba69ba5ccc427198987
Eleve -  Sunil K <ss_apple@ip-192-168-1-206.ap-south-1.compute.internal>
2023-01-19 16:27:40 +0530

    removing all

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-console.log('hello worlting');
-console.log('hello world Greeting');
-console.log('hello world Greeting');
-console.log('hello world Greeting Day');
-console.log('hello world Greeting Day');
-console.log('hello world Greeting Day');
-console.log('hello world 2');
-console.log('hello world 2');
 console.log('hey I am Testing');
