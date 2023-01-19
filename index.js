6ff139262dfca1c1825fa44ad728b32b6545b998
Eleve -  Sunil K <ss_apple@ip-192-168-1-206.ap-south-1.compute.internal>
2023-01-19 14:02:05 +0530

    testing1

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ console.log('hello world Greeting');
 console.log('hello world Greeting Day');
 console.log('hello world Greeting Day');
 console.log('hello world Greeting Day');
+console.log('hello world 2');
