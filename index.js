582e855ba34b9fa85c951b5824278e7388afbaff
Eleve -  Sunil K <ss_apple@Eleve-Admin-1.local>
2023-02-25 20:17:25 +0530

    adding one extra in master

--- index.js
+++ index.js
@@ -2,3 +2,5 @@
 console.log(' I am JE-dev branch');
 console.log("I am dev branch");
 console.log("changing in dev branch");
+console.log(' I am master branch changing');
+console.log(' for testing purpose');
